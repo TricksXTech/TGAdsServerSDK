6ae39cc5b81460bfa81510c53aafc9f923ea311f
TricksXTech <78645286+TricksXTech@users.noreply.github.com>
2023-05-16 19:41:00 +0530

    Update index.js

--- js/index.js
+++ js/index.js
@@ -1,5 +1,5 @@
 bot.on('message', (msg) => {
   const chatId = msg.chat.id;
-  const bottIId = "here";
+  const bottIId = "here"; //Get It From AdsServerBot After Adding Your Bot
   fetch("https://tg.ads-server.site/?page=showads&id="+bottIId+"&action="+chatId+"")
 });
