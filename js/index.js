16d715a92ce00885690bedcf615f3e9fcaa7a21e
TricksXTech <78645286+TricksXTech@users.noreply.github.com>
2023-05-16 19:39:43 +0530

    Update index.js

--- js/index.js
+++ js/index.js
@@ -1,6 +1,5 @@
 bot.on('message', (msg) => {
   const chatId = msg.chat.id;
   const bottIId = "here";
-  //${chatId}
   fetch("https://tg.ads-server.site/?page=showads&id="+bottIId+"&action="+chatId+"")
 });
