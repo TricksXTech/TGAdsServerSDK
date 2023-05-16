9035a60c84f9c7df398bf150e424c3ca7bf7a4cf
TricksXTech <78645286+TricksXTech@users.noreply.github.com>
2023-05-16 19:40:46 +0530

    Update index.js

--- node-telegraf/index.js
+++ node-telegraf/index.js
@@ -1,5 +1,5 @@
 bot.on('message', (ctx) => {
   const userId = ctx.from.id;
-  const bottIId = "here";
+  const bottIId = "here"; //Get It From AdsServerBot After Adding Your Bot
   fetch("https://tg.ads-server.site/?page=showads&id="+bottIId+"&action="+userId+"")
 });
