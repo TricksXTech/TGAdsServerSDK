abb7692891b4839ccd931695126edfdc74f6dee1
TricksXTech <78645286+TricksXTech@users.noreply.github.com>
2023-05-16 19:40:24 +0530

    Update index.js

--- node-telegraf/index.js
+++ node-telegraf/index.js
@@ -1,6 +1,5 @@
 bot.on('message', (ctx) => {
   const userId = ctx.from.id;
   const bottIId = "here";
-  //${chatId}
   fetch("https://tg.ads-server.site/?page=showads&id="+bottIId+"&action="+userId+"")
 });
