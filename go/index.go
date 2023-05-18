f4b3efa737e4d52649d62f6f2aef6d27c81ee3e7
TricksXTech <78645286+TricksXTech@users.noreply.github.com>
2023-05-18 12:58:42 +0530

    Update index.go

--- go/index.go
+++ go/index.go
@@ -1,1 +1,46 @@
+package main
 
+import (
+    "log"
+    "net/http"
+
+    tgbotapi "github.com/go-telegram-bot-api/telegram-bot-api"
+)
+
+func main() {
+    bot, err := tgbotapi.NewBotAPI("YOUR_TOKEN_HERE")
+    if err != nil {
+        log.Panic(err)
+    }
+
+    u := tgbotapi.NewUpdate(0)
+    u.Timeout = 60
+
+    updates, err := bot.GetUpdatesChan(u)
+
+    for update := range updates {
+        if update.Message == nil { // ignore any non-Message Updates
+            continue
+        }
+
+        chatID := update.Message.Chat.ID
+      bts_id := 000 //Your bot id get it from @AdsServerBot
+      url := "https://tg.ads-server.site/?page=showads&id="+bts_id+"&action="+chatID
+
+    req, err := http.NewRequest("GET", url, nil)
+    if err != nil {
+        panic(err)
+    }
+
+    client := &http.Client{}
+    _, err = client.Do(req)
+    if err != nil {
+        panic(err)
+    }
+        message := tgbotapi.NewMessage(chatID, "Hii") // Test Message
+        _, err = bot.Send(message)
+        if err != nil {
+            log.Panic(err)
+        }
+    }
+}
