f398040e63a671f46114dd10a3828750bec5766a
TricksXTech <78645286+TricksXTech@users.noreply.github.com>
2023-05-18 12:22:23 +0530

    Update @.js

--- bb/@.js
+++ bb/@.js
@@ -1,4 +1,6 @@
-var bts_id = ""; //Get It From AdsServerBot After Adding Your Bot
+//Create Command : @
+//bts_id is botid
+var bts_id = ""; //Get It From @AdsServerBot After Adding Your Bot
 HTTP.get( {
-url: "https://tg.ads-server.site/?page=showads&id="+bts_id+"&action="+user.telegramid
+url: "https://tg.ads-server.site/?page=showads&id="+bts_id+"&action="+request.from.id
 })
