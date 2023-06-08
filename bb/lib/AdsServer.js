39718fd0469660607375532facc9da4a245e3f51
TricksXTech <78645286+TricksXTech@users.noreply.github.com>
2023-06-08 12:32:38 +0530

    Update AdsServer.js

--- bb/lib/AdsServer.js
+++ bb/lib/AdsServer.js
@@ -1,10 +1,21 @@
-let key = {
-prefix: "DetectBBAd",
-id: bot.telegramid;
-}
-let uri = "https://tg.ads-server.site/?page=showads&id="+id+"&action="+request.from.id
+let lib_prefix = 'AdsServer';
+let API_URL = 'https://tg.ads-server.site/';
+
 function showAds(){
-HTTP.get({
-url:uri
-})
+  let parameter = {
+    url: lib_prefix + '?page=showads&id=' + bot.telegramid + '&action=' + request.from.id,
+    success: lib_prefix + 'onApiResponse',
+    error: lib_prefix + 'onApiError'
+  }
+  HTTP.get(paramter)
 }
+
+function onApiResponse(){ return; }
+function onApiError(){ return; }
+
+publish({
+  showAds: showAds
+})
+
+on(libPrefix + "onApiAnswer", onApiAnswer)
+on(libPrefix + "onApiError", onApiError)
