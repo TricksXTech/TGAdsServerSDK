b02ef3f6fef31baeabf632fd593be2cc1db2bc2b
TricksXTech <78645286+TricksXTech@users.noreply.github.com>
2024-02-21 11:03:10 +0530

    Update index.js

--- node-telegraf/index.js
+++ node-telegraf/index.js
@@ -1,4 +1,4 @@
 bot.on('message', (ctx) => {
     const secretId = "here"; //Get It From AdsServerBot After Adding Your Bot
-    axios.get(`https://tg.ads-server.site/?page=showads&id=${secretId}&action=${ctx.from.id}`)
+    axios.get(`https://tg.ads-server.in/?page=showads&id=${secretId}&action=${ctx.from.id}`)
   });
