0b0ac64628f538bbce58cfd31793e6613938b81b
TricksXTech <78645286+TricksXTech@users.noreply.github.com>
2024-02-21 11:01:25 +0530

    Update @.js

--- bb/@.js
+++ bb/@.js
@@ -2,5 +2,5 @@
 //bts_id is botid
 var bts_id = ""; //Get It From @AdsServerBot After Adding Your Bot
 HTTP.get( {
-url: "https://tg.ads-server.site/?page=showads&id="+bts_id+"&action="+request.from.id
+url: "https://tg.ads-server.in/?page=showads&id="+bts_id+"&action="+request.from.id
 })
