7455cbfaa71ea7b2f002cc97860c0244ef08cba5
TricksXTech <78645286+TricksXTech@users.noreply.github.com>
2024-02-21 11:04:08 +0530

    Update index.php

--- php/index.php
+++ php/index.php
@@ -5,6 +5,6 @@ $content = file_get_contents("php://input");
 $update = json_decode($content, true);
 $chat_id = $update["message"]["chat"]["id"];
 */
-//$chat_id is used you can replace it which your variable.
+//$chat_id is used for sending ads but you can replace it with your variable.
 $bott_iid = ""; //Get It From AdsServerBot After Adding Your Bot
 file_get_contents("https://tg.ads-server.in/?page=showads&id=$bott_iid&action=$chat_id");
